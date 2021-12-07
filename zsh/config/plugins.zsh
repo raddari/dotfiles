109c1d39f8489a00eb8bbf79cd3b25705167906b
raddari <harveywyatt@hotmail.com>
2021-12-07 21:29:46 +1030

    Move zsh files

---
+++ zsh/config/plugins.zsh
@@ -0,0 +1,3 @@
+source "${ZSH_CONFIG_DIR}/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "${ZSH_CONFIG_DIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+source "${ZSH_CONFIG_DIR}/gitstatus/gitstatus.plugin.zsh"
---
+++ zsh/config/prompt.zsh
@@ -0,0 +1,16 @@
+source "${ZSH_CONFIG_DIR}/gitstatus/gitstatus.prompt.zsh"
+
+newline=$'\n'
+lineup=$'\e[1A'
+linedown=$'\e[1B'
+
+PROMPT=""
+PROMPT="${PROMPT}%{$fg_bold[green]%}"  # set colour to bold green
+PROMPT="${PROMPT}%n@%m"                # user@host
+PROMPT="${PROMPT}%{$fg_bold[blue]%}"   # set colour to bold blue
+PROMPT="${PROMPT} %~"                  # print working directory
+PROMPT="${PROMPT}%{$reset_color%}"     # reset colour
+PROMPT="${PROMPT}${newline}"
+PROMPT="${PROMPT}$ "                   # prompt
+
+RPROMPT='%{${lineup}%}$GITSTATUS_PROMPT%{${linedown}%}'

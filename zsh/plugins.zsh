d22745251e0968684d19dab9d085f549a47ac88a
raddari <harveywyatt@hotmail.com>
2021-12-07 21:32:56 +1030

    Fix zsh git locations

--- zsh/plugins.zsh
+++
@@ -1,3 +0,0 @@
-source "${ZSH_CONFIG_DIR}/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "${ZSH_CONFIG_DIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-source "${ZSH_CONFIG_DIR}/gitstatus/gitstatus.plugin.zsh"

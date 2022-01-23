1c3bbbf64358afb8712ddd82c6255181968f2558
raddari <harveywyatt@hotmail.com>
2022-01-23 17:26:22 +1030

    Reorder install script

--- install.sh
+++ install.sh
@@ -6,17 +6,16 @@ if [ "$1" = "-y" ]; then
   ln_args="-sf"
 fi
 
-mkdir -p ~/.zsh/config
-mkdir -p ~/.zsh/plugins
-mkdir -p ~/.config/nvim/lua
-
 echo "Installing submodules"
 git submodule update --init --recursive
 
 echo "Symlinking files and directories"
+mkdir -p ~/.config/nvim/lua
 ln $ln_args ${base_dir}/nvim/init.lua ~/.config/nvim/init.lua
 ln $ln_args ${base_dir}/nvim/lua/* ~/.config/nvim/lua/
 
+mkdir -p ~/.zsh/config
+mkdir -p ~/.zsh/plugins
 ln $ln_args ${base_dir}/zsh/zshrc ~/.zshrc
 ln $ln_args ${base_dir}/zsh/config/* ~/.zsh/config
 ln $ln_args ${base_dir}/zsh/plugins/* ~/.zsh/plugins

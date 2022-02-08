85162aecec110ee461a45f5b6904954fd4cfe8bc
raddari <harveywyatt@hotmail.com>
2022-02-08 23:14:24 +1030

    Add colours to install script :)

--- install.fish
+++ install.fish
@@ -1,4 +1,8 @@
 #!/usr/bin/env fish
+set -l c_reset "\033[0m"
+set -l c_yellow "\033[1;33m"
+set -l c_lgreen "\033[1;32m"
+
 function group_data_link --description "group_data_link <group> <sources...>"
   set -l base_dir (dirname (readlink -m (status --current-filename)))
   set -l data_dir "$HOME/.config"
@@ -20,8 +24,8 @@ function data_link --description "data_link <source> <link>"
   ln -s "$argv[1]" "$argv[2]"
 end
 
-echo "Symlinking files and directories"
+printf "[%bSymlinking files and directories%b]\n" $c_yellow $c_reset
 group_data_link "nvim" "init.lua" "lua"
 group_data_link "fish" "config.fish" "functions" "user"
 
-echo "Done!"
+printf "[%bDone!%b]\n" $c_lgreen $c_reset

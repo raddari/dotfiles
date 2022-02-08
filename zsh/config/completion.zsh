a0bea307a4dbd3197cd1cc2cf76e0adba0118654
raddari <harveywyatt@hotmail.com>
2022-02-08 21:42:47 +1030

    Remove zsh and submodules

--- zsh/config/completion.zsh
+++
@@ -1,9 +0,0 @@
-zstyle ':completion:*' menu select
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-zstyle ':completion:*' rehash true
-zstyle ':completion:*' accept-exact '*(N)'
-
-zstyle :compinstall filename "$HOME/.zshrc"
-autoload -U compinit
-compinit -d
--- zsh/config/plugins.zsh
+++
@@ -1,3 +0,0 @@
-source "${ZSH_PLUGIN_DIR}/zsh-autosuggestions/zsh-autosuggestions.zsh"
-source "${ZSH_PLUGIN_DIR}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-source "${ZSH_PLUGIN_DIR}/gitstatus/gitstatus.plugin.zsh"

1637b7e40c4ec0d52e76f790ccb8a1f47c994cd0
raddari <harveywyatt@hotmail.com>
2022-02-08 23:44:06 +1030

    Use fish colours for install script

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,16 @@
 function fish_prompt
+  set -g fish_prompt_pwd_dir_length 4
   if test -n "$SSH_TTY"
-    echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+    echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)
   end
 
-  echo -n (set_color blue)(prompt_pwd)' '
+  echo -n (set_color blue)(prompt_pwd)
 
   set_color -o
   if fish_is_root_user
-    echo -n (set_color red)'# '
+    echo -n (set_color red)'#'
   end
+  echo "" # Newline baby
   echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
   set_color normal
 end
--- install.fish
+++ install.fish
@@ -1,8 +1,4 @@
 #!/usr/bin/env fish
-set -l c_reset "\033[0m"
-set -l c_yellow "\033[1;33m"
-set -l c_lgreen "\033[1;32m"
-
 function group_data_link --description "group_data_link <group> <sources...>"
   set -l base_dir (dirname (readlink -m (status --current-filename)))
   set -l data_dir "$HOME/.config"
@@ -24,8 +20,9 @@ function data_link --description "data_link <source> <link>"
   ln -s "$argv[1]" "$argv[2]"
 end
 
-printf "[%bSymlinking files and directories%b]\n" $c_yellow $c_reset
+echo "["(set_color yellow)"Symlinking files and directories"(set_color normal)"]"
+
 group_data_link "nvim" "init.lua" "lua"
 group_data_link "fish" "config.fish" "functions" "user"
 
-printf "[%bDone!%b]\n" $c_lgreen $c_reset
+echo "["(set_color brgreen)"Done!"(set_color normal)"]"

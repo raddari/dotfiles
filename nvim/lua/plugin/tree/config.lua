5651ea3a3a3a466257ced18c5f0b46efd8321e3c
raddari <harveywyatt@hotmail.com>
2022-03-02 15:06:17 +1030

    Move quit_on_open to setup

--- nvim/lua/plugin/tree/config.lua
+++ nvim/lua/plugin/tree/config.lua
@@ -1,11 +1,15 @@
 local g = vim.g
 
 -- TODO(raddari): move these to setup when they are relocated
-g.nvim_tree_quit_on_open = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1}
 
 require('nvim-tree').setup({
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
   filters = {
     custom = {'.git'}
   },

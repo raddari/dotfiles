dbc74b4bd37a33a8117c87107eb72c7dfbeb03eb
raddari <harveywyatt@hotmail.com>
2022-05-01 23:18:28 +0930

    Update treesitter config

--- nvim/lua/plugin/treesitter/config.lua
+++ nvim/lua/plugin/treesitter/config.lua
@@ -1,5 +1,15 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = 'maintained',
+  ensure_installed = {
+    "bash",
+    "c",
+    "cpp",
+    "fish",
+    "glsl",
+    "java",
+    "lua",
+    "python",
+    "rust",
+  },
   highlight = {
     enable = true,
   },

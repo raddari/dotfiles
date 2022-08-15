3c353652c8f1c69508d42e3717baa73c7ffe7ed9
raddari <harveywyatt@hotmail.com>
2022-08-16 02:40:46 +0930

    Update default formatting

--- nvim/lua/lsp/config/sumneko_lua.lua
+++ nvim/lua/lsp/config/sumneko_lua.lua
@@ -12,6 +12,10 @@ return {
       },
       format = {
         enable = true,
+        defaultConfig = {
+          quote_style = 'single',
+          space_before_function_open_parenthesis = true,
+        },
       },
       telemetry = {
         enable = false,

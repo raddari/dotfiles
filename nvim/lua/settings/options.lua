14692e18b089c988c23c426889b482ec11218b37
raddari <harveywyatt@hotmail.com>
2022-08-17 21:16:33 +0930

    Adjust options

--- nvim/lua/settings/options.lua
+++ nvim/lua/settings/options.lua
@@ -3,9 +3,19 @@ local opt = vim.opt
 opt.completeopt = { 'menuone', 'noinsert', 'noselect' }
 opt.dir = '/tmp'
 opt.expandtab = true
+opt.fillchars:append({
+  horiz = '━',
+  horizup = '┻',
+  horizdown = '┳',
+  vert = '┃',
+  vertleft = '┨',
+  vertright = '┣',
+  verthoriz = '╋',
+})
 opt.hidden = true
 opt.ignorecase = true
 opt.joinspaces = true
+opt.laststatus = 3
 opt.list = true
 opt.number = true
 opt.relativenumber = true
@@ -13,6 +23,7 @@ opt.scrolloff = 4
 opt.shiftround = true
 opt.shiftwidth = 2
 opt.sidescrolloff = 8
+opt.signcolumn = 'yes:1'
 opt.smartcase = true
 opt.smartindent = true
 opt.splitbelow = true

97e9b104a603c36633b3590352c8d4bc1b85a3b2
raddari <harveywyatt@hotmail.com>
2022-08-20 23:43:29 +0930

    Add telescope-file-browser

--- config/nvim/lua/plugin/telescope/keymap.lua
+++ config/nvim/lua/plugin/telescope/keymap.lua
@@ -1,14 +1,8 @@
 local keymap = vim.keymap
 
-local telescope = {}
-setmetatable(telescope, {
-  __index = function(_, key)
-    return require('telescope.' .. key)
-  end
-})
-
-keymap.set('n', '<Leader>ff', telescope.builtin.git_files)
-keymap.set('n', '<Leader>fa', telescope.builtin.find_files)
-keymap.set('n', '<Leader>fg', telescope.builtin.live_grep)
-keymap.set('n', '<Leader>fb', telescope.builtin.buffers)
-keymap.set('n', '<Leader>fh', telescope.builtin.help_tags)
+keymap.set('n', '<Leader>ff', require('telescope.builtin').git_files)
+keymap.set('n', '<Leader>fa', require('telescope.builtin').find_files)
+keymap.set('n', '<Leader>fg', require('telescope.builtin').live_grep)
+keymap.set('n', '<Leader>fb', require('telescope.builtin').buffers)
+keymap.set('n', '<Leader>fh', require('telescope.builtin').help_tags)
+keymap.set('n', '<Leader>ft', require('telescope').extensions.file_browser.file_browser)

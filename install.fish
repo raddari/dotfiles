3c7fdb7f4de6b04b4a2859337f3f4d6c4afef87c
raddari <harveywyatt@hotmail.com>
2022-08-23 19:37:32 +0930

    Reindent install script

--- install.fish
+++ install.fish
@@ -1,18 +1,18 @@
 #!/usr/bin/env fish
 
 function link_module
-  set -f module "$argv[1]"
-  set -f source_dir "$argv[2]"
-  set -f dest_dir "$XDG_CONFIG_HOME/$module"
-  mkdir -p "$dest_dir"
-  ln -svi $source_dir/* $dest_dir
+    set -f module "$argv[1]"
+    set -f source_dir "$argv[2]"
+    set -f dest_dir "$XDG_CONFIG_HOME/$module"
+    mkdir -p "$dest_dir"
+    ln -svi $source_dir/* $dest_dir
 end
 
 set dotfiles_dir (dirname (readlink -m (status --current-filename)))
 source "$dotfiles_dir/config/fish/conf.d/000-env.fish"
 
-set -l modules "fish" "nvim" "tmux" "clangd"
+set -l modules fish nvim tmux clangd
 for module in $modules
-  # TODO(raddari): split into own install files
-  link_module "$module" "$dotfiles_dir/config/$module"
+    # TODO(raddari): split into own install files
+    link_module "$module" "$dotfiles_dir/config/$module"
 end
